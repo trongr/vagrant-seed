e0e29f75b6af0bea51e4180174bec573bf34ad18
trong <turongr@gmail.com>
2014-11-07 20:27:51 -0500

    more provisioning with debconfutils

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -3,6 +3,7 @@
 DBNAME=testdb
 DBUSER=testuser
 DBPASS=testpass
+ROOTDIR=/home/vagrant/nt
 
 echo "provisioning vm"
 echo "installing packages"
@@ -17,19 +18,19 @@ apt-get install -y git
 
 echo "installing nginx"
 apt-get install -y nginx
-echo "TODO. copying nginx configs"
-# cp configs/nginx/conf.d/* /etc/nginx/conf.d/
+mv /etc/nginx/sites-enabled/default $ROOTDIR/provision/nginx/sites-enabled/
 echo "restarting nginx server"
 service nginx restart
 
-# TODO. uncomment. don't actually need mysql, just wanted to try out debconfutils
-echo "debconf utils configuring mysql server password"
-debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASS"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASS"
-echo "installing mysql client mysql server"
-apt-get install -y mysql-server mysql-client
-echo "creating database and granting privileges"
-mysql -u root -p $DBPASSWD -e "CREATE DATABASE $DBNAME"
-mysql -u root -p $DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASS'"
-echo "restarting mysql server"
-service mysql restart
+# # TODO. comment. don't actually need mysql, just wanted to try out debconfutils
+# echo "debconf utils configuring mysql server password"
+# debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASS"
+# debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASS"
+# echo "installing mysql client mysql server"
+# apt-get install -y mysql-server mysql-client
+# echo "creating database and granting privileges"
+# # NOTE -uroot -p$DBPASS and not -u root -p $DBPASS
+# mysql -uroot -p$DBPASS -e "CREATE DATABASE $DBNAME"
+# mysql -uroot -p$DBPASS -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASS'"
+# echo "restarting mysql server"
+# service mysql restart
